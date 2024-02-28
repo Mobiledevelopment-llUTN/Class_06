c65436a486b97517ae51b788df4f4f28a9334f0d
LilMat69 <semataoe@est.utn.ac.cr>
2024-02-27 23:25:05 -0600

    Login & SignUp Views Navegation

--- App.tsx
+++ App.tsx
@@ -1,12 +1,34 @@
+/* call react libraries */
 import React from 'react';
-import {View} from 'react-native';
-import ValiUser from './src/components/fb_Valiusers';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+/* custom modules */
+import Login from './src/views/login';
+import SignUp from './src/views/signup';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <View>
-      <ValiUser />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='LogIn'>
+        <Stack.Screen
+          name="LogIn"
+          component={Login}
+          options={{
+            headerShown: false
+          }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUp}
+          options={{
+            headerShown:false
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
